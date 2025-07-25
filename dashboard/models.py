c72e18591f09cec5d33c22ef07b21f561d334b8d
guesswho-jr <guess.who.jr.2000@gmail.com>
2025-07-25 14:19:06 +0300

    This is the working code. Everything is good.

--- dashboard/models.py
+++ dashboard/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=100)
     product_description = models.CharField(max_length=512)
     product_original_price = models.DecimalField(decimal_places=2, max_digits=12)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     product_initial_time = models.DateTimeField(help_text=_("The time when the product is added to stock"), auto_now_add=True)
     discount_amount = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal(0.00))
     product_amount = models.PositiveIntegerField(default=0)
@@ -28,10 +28,10 @@ class Product(models.Model):
     @property
     def is_discounted(self):
         return True if self.product_original_price - self.discount_amount > 0 else False
-    # @property
-    # def vendor_image(self):
-    #     print(self.user)
-    #     up = UserProfile(user=self.user)
-    #     print(up.profile_picture)
-    #     print("----")
-    #     # return up.profile_picture
+    @property
+    def vendor_image(self):
+        print(self.user)
+        up = UserProfile(user=self.user)
+        print(up.profile_picture)
+        print("----")
+        # return up.profile_picture
