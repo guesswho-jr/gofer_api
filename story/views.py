fa6c986f002e971bbd5ea03b88cad245cf913119
guesswho-jr <guess.who.jr.2000@gmail.com>
2025-08-22 08:29:05 +0300

    Added user filter and the stories (exlclude user) added an ip into the allowed_host and changed the stories view to get the user id

--- story/views.py
+++ story/views.py
@@ -2,13 +2,21 @@ from rest_framework import generics
 from rest_framework.response import Response
 from story.serializers import StoryCreateUpdateSerializer, StorySerializer
 from rest_framework.pagination import PageNumberPagination
-from .models import Story
+from .models import Story, User
 from django.utils import timezone
-
+from rest_framework_simplejwt.tokens import AccessToken
 class StoryListView(generics.ListAPIView):
-    queryset = Story.objects.filter(created_at__gt = timezone.now() - timezone.timedelta(days=1))
     serializer_class = StorySerializer
     pagination_class = PageNumberPagination
+    def get_user_from_token(self):
+        raw_token = self.request.headers["Authorization"].split(" ")[1]
+        t = AccessToken(raw_token)
+        user = User.objects.get(pk=t["user_id"])
+        return user
+    def get_queryset(self): # type: ignore
+        data = Story.objects.filter(created_at__gt = timezone.now() - timezone.timedelta(days=1)).filter(uploaded_by=self.get_user_from_token())
+        return data
+        # return super().get_queryset()
     
 class StoryRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Story.objects.filter(created_at__gt = timezone.now() - timezone.timedelta(days=1))
